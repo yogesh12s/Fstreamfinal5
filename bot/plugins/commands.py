a96772f11b9efbd0a3105cef4ae0a26b8a83059d
yogesh12s <yogeshgokul372@gmail.com>
2026-08-08 15:17:51 +0530

    Refactor commands to use hydrogram Client

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -1,23 +1,22 @@
-from hydrogram import filters
+from hydrogram import Client, filters
 from hydrogram.types import Message
-from bot import TelegramBot
 from bot.config import Telegram
 from bot.modules.static import *
 from bot.modules.decorators import verify_user
 
-@TelegramBot.on_message(filters.command(['start', 'help']) & filters.private)
+@Client.on_message(filters.command(['start', 'help']) & filters.private)
 @verify_user
-async def start_command(_, msg: Message):
+async def start_command(client: Client, msg: Message):
     await msg.reply(
         text = WelcomeText % {'first_name': msg.from_user.first_name},
         quote = True
     )
 
-@TelegramBot.on_message(filters.command('privacy') & filters.private)
+@Client.on_message(filters.command('privacy') & filters.private)
 @verify_user
-async def privacy_command(_, msg: Message):
+async def privacy_command(client: Client, msg: Message):
     await msg.reply(text=PrivacyText, quote=True, disable_web_page_preview=True)
 
-@TelegramBot.on_message(filters.command('log') & filters.chat(Telegram.OWNER_ID))
-async def log_command(_, msg: Message):
+@Client.on_message(filters.command('log') & filters.chat(Telegram.OWNER_ID))
+async def log_command(client: Client, msg: Message):
     await msg.reply_document('event-log.txt', quote=True)
